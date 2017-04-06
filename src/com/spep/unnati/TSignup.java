f1b43cbf889c95ac02a9ac0677ee14bef4b663b5
Apurv Singh Gautam <apurvsinghgautam@gmail.com>
2017-04-06 20:56:34 +0530

    Session Applied

--- src/com/spep/unnati/TSignup.java
+++ src/com/spep/unnati/TSignup.java
@@ -11,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class ESignup
@@ -43,18 +44,20 @@ public class TSignup extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://appuscorpion.me:3306/alognew", "root",
-					"scorpion");
-			PreparedStatement ps = conn.prepareStatement("INSERT INTO company VALUES(?,?,?,?,?)");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/alognew", "root", "scorpion");
+			PreparedStatement ps = conn.prepareStatement("INSERT INTO logintb (name,mobile,password) VALUES(?,?,?)");
 			ps.setString(1, organization);
 			ps.setString(2, contact);
 			ps.setString(3, zip);
-			ps.setString(4, country);
-			ps.setString(5, state);
+			// ps.setString(4, country);
+			// ps.setString(5, state);
 			int i = ps.executeUpdate();
 
 			if (i > 0) {
 				out.println("You are successfully Registered!");
+				HttpSession session = request.getSession();
+				session.setAttribute("Organization", organization);
+				response.sendRedirect("login.html");
 			}
 
 		} catch (Exception ex) {
