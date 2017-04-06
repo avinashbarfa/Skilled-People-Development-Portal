b6cc23b1d4757e48ac08b70edf98f298f4b43e04
AVINASH BARFA <avibarfa98@gmail.com>
2017-04-06 23:58:49 +0530

    Add labour backed is ready.

--- src/com/spep/unnati/AddLabour.java
+++ src/com/spep/unnati/AddLabour.java
@@ -13,53 +13,56 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class ESignup
+ * Servlet implementation class AddLabour
  */
-@WebServlet("/TSignup")
-public class TSignup extends HttpServlet {
+@WebServlet("/AddLabour")
+public class AddLabour extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddLabour() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
 	/**
-	 * @see HttpServlet#HttpServlet()
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	public TSignup() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.setContentType("text/html");
-		String organization = request.getParameter("firstname");
-		String contact = request.getParameter("lastname");
+		String name = request.getParameter("name");
+		String contact = request.getParameter("contact");
 		String zip = request.getParameter("zip");
+		String gender = request.getParameter("state");
+		String skill = request.getParameter("skill");
 		String country = request.getParameter("country");
 		String state = request.getParameter("state");
 		PrintWriter out = response.getWriter();
+		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection conn = DriverManager.getConnection("jdbc:mysql://appuscorpion.me:3306/alognew", "root", "scorpion");
-			PreparedStatement ps = conn.prepareStatement("INSERT INTO company VALUES(?,?,?,?,?)");
-			ps.setString(1, organization);
+			PreparedStatement ps =conn.prepareStatement("");
+			ps.setNString(1, name);
 			ps.setString(2, contact);
 			ps.setString(3, zip);
-			ps.setString(4, country);
-			ps.setString(5, state);
+			ps.setString(3, gender);
+			ps.setString(4, skill);
+			ps.setString(5, country);
+			ps.setNString(6, state);
+			
 			int i = ps.executeUpdate();
-
-			if (i > 0) {
-				out.println("You are successfully Registered!");
+			
+			if(i > 0) {
+				out.println("Labour Is Successfull Registered !");
 			}
-
 		} catch (Exception ex) {
-			System.out.println(ex);
+			System.out.print(ex);
 		}
-
+		
 	}
 
 }
