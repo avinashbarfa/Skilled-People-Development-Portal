b164171e12bd95e0218a5733055a07f9fe761b5f
Apurv Singh Gautam <apurvsinghgautam@gmail.com>
2017-05-01 16:30:32 +0530

    Working Location Based Search

--- src/com/spep/unnati/CompanySearchLabour.java
+++ src/com/spep/unnati/CompanySearchLabour.java
@@ -40,29 +40,26 @@ public class CompanySearchLabour extends HttpServlet {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		double clongitude = 0;
-        double clatitude = 0;
+		double clatitude = 0;
 		String nameOfCompany = request.getParameter("cname");
 		String skill = request.getParameter("skill-wise");
-		// System.out.println(name);
+		// System.out.println(nameOfCompany);
 		int numberOfPeople = Integer.parseInt(request.getParameter("nop"));
-		// System.out.println(number);
 		ArrayList<String> al = new ArrayList<>();
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/unnati","root", "root");
-			PreparedStatement ps = conn.prepareStatement("SELECT compname,latitude,longitude from company where compname=?");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/unnati", "root", "root");
+			PreparedStatement ps = conn
+					.prepareStatement("SELECT compname,latitude,longitude from company where compname=?");
 			ps.setString(1, nameOfCompany);
 			ResultSet rs = ps.executeQuery();
-			out.println(numberOfPeople);
-			out.println(skill);
-			out.println(nameOfCompany);
-			if (rs.next()) { 
-				clongitude = Double.parseDouble(rs.getString("longitude")); //77.5946;
-				clatitude = Double.parseDouble(rs.getString("latitude")); //12.9716;
+			if (rs.next()) {
+				clongitude = Double.parseDouble(rs.getString("longitude")); // 77.5946;
+				clatitude = Double.parseDouble(rs.getString("latitude")); // 12.9716;
 			}
-			
-			al = locator(clongitude,clatitude,numberOfPeople,skill);
-			int count = 1;
+
+			al = locator(clongitude, clatitude, numberOfPeople, skill);
+			// int count = 1;
 			Iterator<String> it = al.iterator();
 			while (it.hasNext()) {
 				String[] splited = it.next().split("\\s+");
@@ -72,81 +69,85 @@ public class CompanySearchLabour extends HttpServlet {
 			}
 
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println(e);
 		}
 
 	}
-	
-	String query(double clongitude,double clatitude,int dist,String skill){
-		
+
+	String query(double clongitude, double clatitude, int dist) {
+
 		double latitudeDist = clatitude * 111194.9266;
 		double longitudeDist = clongitude * 111194.9266;
-		
-	    double minLatitudeDist = latitudeDist - dist;
-	    String minLatitude = String.valueOf(minLatitudeDist / 111194.9266);
-	    double minLongitudeDist = longitudeDist - dist;
-	    String minLongitude = String.valueOf(minLongitudeDist / 111194.9266);
-	    double maxLatitudeDist = latitudeDist + dist;
-	    String maxLatitude = String.valueOf(maxLatitudeDist / 111194.9266);
-	    double maxLongitudeDist = longitudeDist + dist;
-	    String maxLongitude = String.valueOf(maxLongitudeDist / 111194.9266);
-	    String query = "SELECT id,fullname,contact,latitude,longitude from labour WHERE longitude <= " + maxLongitude + " and longitude >= " + minLongitude + " and latitude <= " + maxLatitude + " and latitude >= " + minLatitude + " and jobstatus = Not Employed and skill = " + skill;
-	    return query;
+		double minLatitudeDist = latitudeDist - dist;
+		String minLatitude = String.valueOf(minLatitudeDist / 111194.9266);
+		double minLongitudeDist = longitudeDist - dist;
+		String minLongitude = String.valueOf(minLongitudeDist / 111194.9266);
+		double maxLatitudeDist = latitudeDist + dist;
+		String maxLatitude = String.valueOf(maxLatitudeDist / 111194.9266);
+		double maxLongitudeDist = longitudeDist + dist;
+		String maxLongitude = String.valueOf(maxLongitudeDist / 111194.9266);
+		String query = "SELECT id,fullname,contact,latitude,longitude from labour WHERE longitude <= " + maxLongitude
+				+ " and longitude >= " + minLongitude + " and latitude <= " + maxLatitude + " and latitude >= "
+				+ minLatitude + " and jobstatus = ? and skill = ?";
+		return query;
 	}
-	
-	ArrayList<String> locator(double clongitude,double clatitude,int numberOfPeople,String skill) {
+
+	ArrayList<String> locator(double clongitude, double clatitude, int numberOfPeople, String skill) {
 
 		ArrayList<String> al = new ArrayList<>();
-		
+
 		int dist = 70;// Hundred Kilometers radius.
-		
+
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/unnati", "root", "root");
-			
-			int count = 0;
-			
-			while(count <= numberOfPeople){
-				
-			    count = 0;
-				
-				PreparedStatement ps1 = conn.prepareStatement(query(clongitude,clatitude,dist,skill));
-				ResultSet rs1 = ps1.executeQuery();
-				
-			    while(rs1.next()){
-				    count++;
-			    }
-			    
-			    if(count <= numberOfPeople){
-			    	dist += 70;
-			    }
-			    
-			}
-			
 
-			PreparedStatement ps2 = conn.prepareStatement(query(clongitude,clatitude,dist,skill));
+			int count = 0;
+
+			while (count <= numberOfPeople) {
+
+				count = 0;
+
+				PreparedStatement ps1 = conn.prepareStatement(query(clongitude, clatitude, dist));
+				ps1.setString(1, "Not Employed");
+				ps1.setString(2, skill);
+				ResultSet rs1 = ps1.executeQuery();
+
+				while (rs1.next()) {
+					count++;
+				}
+
+				if (count <= numberOfPeople) {
+					dist += 70;
+				}
+
+			}
+
+			PreparedStatement ps2 = conn.prepareStatement(query(clongitude, clatitude, dist));
+			ps2.setString(1, "Not Employed");
+			ps2.setString(2, skill);
 			ResultSet rs2 = ps2.executeQuery();
-			
+
 			int count2 = 0;
-			
+
 			while (rs2.next()) {
-				
+
 				String name = rs2.getString(2);
 				String contact = rs2.getString(3);
 				String concat = name + " " + contact;
 				al.add(concat);
 				count2++;
-				
-				if(count2 == numberOfPeople){
+
+				if (count2 == numberOfPeople) {
 					break;
 				}
-				
+
 			}
-			
-		} catch(Exception e) {
-			e.printStackTrace();
+
+		} catch (Exception e) {
+			System.out.println(e);
 		}
-		
+
 		return al;
 	}
 }
